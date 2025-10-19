04cf45248fe218d89d6497e3ad8222126ab6a975
khalid-jamal <jamalkhalid544@gmail.com>
2025-10-19 12:34:33 +0530

    Initial

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'shop.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- shop/models.py
+++ shop/models.py
@@ -7,11 +7,29 @@ ORDER_STATUS_CHOICES = [
     ('DELIVERED', 'Delivered'),
 ]
 
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     address = models.TextField(blank=True)
     phone = models.CharField(max_length=20, blank=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_set",
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
