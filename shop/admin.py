23e0029db3811ad709d42492b9767195087e3590
khalid-jamal <jamalkhalid544@gmail.com>
2025-10-19 13:13:42 +0530

    Initial

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User , Category , Product, CartItem , Order , OrderItem
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
