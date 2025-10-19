e9d4a050ee854f2b505b7310f0e710264c422fd7
khalid-jamal <jamalkhalid544@gmail.com>
2025-10-19 14:30:19 +0530

    Initial

--- e_commerce/asgi.py
+++ e_commerce/asgi.py
@@ -1,9 +1,11 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import shop.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
